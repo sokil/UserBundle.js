642d618a142086b3c7082a1640885b3981ee8500
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-08-17 23:26:40 +0300

    test is router

--- tests/IntegrationSpec.js
+++ tests/IntegrationSpec.js
@@ -1,12 +1,12 @@
 describe("UserBundle Integration Test Suite", function() {
 
-    var UserBundle = require('../dist/user');
+    var UserRouter = require('../dist/user');
     var jquery = require('../node_modules/jquery/dist/jquery');
     var Backbone = require('../node_modules/backbone/backbone');
     var Marionette = require('../node_modules/backbone.marionette/lib/backbone.marionette');
 
     it("Ititialise user bundle", function() {
-        var userBundle = new UserBundle();
-        expect(userBundle).toEqual(jasmine.any(UserBundle));
+        var userRouter = new UserRouter();
+        expect(userRouter.routes).not.toBeNull();
     });
 });
